fb50194c25ac5c80be62ffd236c993c5f140e376
Jordan Sleeper <ezirens@gmail.com>
2016-12-22 20:00:01 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
+      t.boolean :family_friendly
+
+      t.timestamps null: false
+    end
+  end
+end
